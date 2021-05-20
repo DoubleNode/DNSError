1c11ce7df4ec14b8b94a2f32cb5d557c8c91ab45
Darren Ehlers <me@darrenehlers.com>
2021-05-20 13:34:11 -0500

    test

--- buildFramework.sh
+++ buildFramework.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
-xcodebuild archive -scheme DNSError-iOS -destination "generic/platform=iOS" -archivePath ./Archives/iOS/DNSError SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme DNSError-iOS -destination "generic/platform=iOS" \
+    -archivePath ./Archives/iOS/DNSErrorFramework SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-xcodebuild archive -scheme DNSError-iOS -destination "generic/platform=iOS Simulator" -archivePath ./Archives/Simulator/DNSError SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme DNSError-iOS -destination "generic/platform=iOS Simulator" \
+    -archivePath ./Archives/Simulator/DNSErrorFramework SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-xcodebuild archive -scheme DNSError-macOS -destination "generic/platform=macOS" -archivePath ./Archives/macOS/DNSError SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme DNSError-macOS -destination "generic/platform=macOS" \
+    -archivePath ./Archives/macOS/DNSErrorFramework SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-rm -rf ./Archives/DNSError.xcframework
+rm -rf ./Archives/DNSErrorFramework.xcframework
+
 xcodebuild -create-xcframework \
--framework ./Archives/iOS/DNSError.xcarchive/Products/Library/Frameworks/DNSError_iOS.framework \
--framework ./Archives/Simulator/DNSError.xcarchive/Products/Library/Frameworks/DNSError_iOS.framework \
--framework ./Archives/macOS/DNSError.xcarchive/Products/Library/Frameworks/DNSError_macOS.framework \
--output ./Archives/DNSError.xcframework
+    -framework ./Archives/iOS/DNSErrorFramework.xcarchive/Products/Library/Frameworks/DNSError_iOS.framework \
+    -framework ./Archives/Simulator/DNSErrorFramework.xcarchive/Products/Library/Frameworks/DNSError_iOS.framework \
+    -framework ./Archives/macOS/DNSErrorFramework.xcarchive/Products/Library/Frameworks/DNSError_macOS.framework \
+    -output ./Archives/DNSErrorFramework.xcframework
