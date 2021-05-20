b2b73ed2890a1424521da9347a9807dd396a8852
Darren Ehlers <me@darrenehlers.com>
2021-05-20 14:01:13 -0500

    test

---
+++ Archives/DNSErrorFramework.xcframework/ios-arm64_x86_64-simulator/DNSErrorFramework_iOS.framework/Headers/DNSErrorFramework_iOS.h
@@ -0,0 +1,16 @@
+//
+//  DNSErrorFramework_iOS.h
+//  DNSErrorFramework-iOS
+//
+//  Created by Darren.Ehlers on 5/19/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DNSErrorFramework_iOS.
+FOUNDATION_EXPORT double DNSErrorFramework_iOSVersionNumber;
+
+//! Project version string for DNSErrorFramework_iOS.
+FOUNDATION_EXPORT const unsigned char DNSErrorFramework_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DNSErrorFramework_iOS/PublicHeader.h>
--- buildFramework.sh
+++ buildFramework.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-xcodebuild archive -scheme DNSError-iOS -destination "generic/platform=iOS" \
+xcodebuild archive -scheme DNSErrorFramework-iOS -destination "generic/platform=iOS" \
     -archivePath ./Archives/iOS/DNSErrorFramework SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-xcodebuild archive -scheme DNSError-iOS -destination "generic/platform=iOS Simulator" \
+xcodebuild archive -scheme DNSErrorFramework-iOS -destination "generic/platform=iOS Simulator" \
     -archivePath ./Archives/Simulator/DNSErrorFramework SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-xcodebuild archive -scheme DNSError-macOS -destination "generic/platform=macOS" \
+xcodebuild archive -scheme DNSErrorFramework-macOS -destination "generic/platform=macOS" \
     -archivePath ./Archives/macOS/DNSErrorFramework SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 rm -rf ./Archives/DNSErrorFramework.xcframework
 
 xcodebuild -create-xcframework \
-    -framework ./Archives/iOS/DNSErrorFramework.xcarchive/Products/Library/Frameworks/DNSError_iOS.framework \
-    -framework ./Archives/Simulator/DNSErrorFramework.xcarchive/Products/Library/Frameworks/DNSError_iOS.framework \
-    -framework ./Archives/macOS/DNSErrorFramework.xcarchive/Products/Library/Frameworks/DNSError_macOS.framework \
+    -framework ./Archives/iOS/DNSErrorFramework.xcarchive/Products/Library/Frameworks/DNSErrorFramework_iOS.framework \
+    -framework ./Archives/Simulator/DNSErrorFramework.xcarchive/Products/Library/Frameworks/DNSErrorFramework_iOS.framework \
+    -framework ./Archives/macOS/DNSErrorFramework.xcarchive/Products/Library/Frameworks/DNSErrorFramework_macOS.framework \
     -output ./Archives/DNSErrorFramework.xcframework
